3a9777565cb56d31069c8a8bdb4e907889d87a72
Elmar <Eva.Electronics@outlook.com>
2013-08-12 01:55:56 +0200

    Added Logger file, added socketutil. added Configuration (todo, read from external file).

---
+++ RTS-desktop/src/com/rts/util/Configuration.java
@@ -0,0 +1,14 @@
+package com.rts.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Elmar
+ * Date: 8/12/13
+ * Time: 1:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Configuration {
+    public static final String LOG_DIRECTORY = "/";
+    public static final int LOG_LEVEL = Logger.ALL;
+    public static final boolean LOG_TO_FILE = false;
+}
