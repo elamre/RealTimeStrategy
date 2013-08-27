34d7b5ea9931a402e13e64dd0dfe990d44fd7390
dermetfan <serverkorken@googlemail.com>
2013-08-27 14:35:47 +0200

    started to make a main menu. nothing really in now but will continue later

---
+++ RTS/src/com/rts/game/screens/Menu.java
@@ -0,0 +1,50 @@
+package com.rts.game.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+/** TODO this is going to be the main menu */ // will continue later, I have to go now
+public class Menu implements Screen {
+
+	private Skin skin;
+	private Stage stage = new Stage();
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		stage.act(delta);
+		stage.draw();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		stage.setViewport(width, height, false);
+	}
+
+	@Override
+	public void show() {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+	}
+
+	@Override
+	public void hide() {
+	}
+
+	@Override
+	public void pause() {
+	}
+
+	@Override
+	public void resume() {
+	}
+
+	@Override
+	public void dispose() {
+		stage.dispose();
+	}
+
+}
