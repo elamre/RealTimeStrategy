152072f98b9eda14edbb1e8fbadde264c137a6d4
Jacob Laurendeau <jakellaurendeau@gmail.com>
2013-08-30 15:15:01 -0400

    Changed Game class so that the game would run. If this was messing with someone else's work, sorry.

--- RTS-desktop/src/com/rts/game/Main.java
+++ RTS-desktop/src/com/rts/game/Main.java
@@ -2,14 +2,15 @@ package com.rts.game;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rts.game.screens.Game;
 
 public class Main {
-	public static void main(String[] args) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = "RTS";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
-		new LwjglApplication(new Game(), cfg);
-	}
+    public static void main(String[] args) {
+        LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+        cfg.title = "RTS";
+        cfg.useGL20 = false;
+        cfg.width = 480;
+        cfg.height = 320;
+        new LwjglApplication(new Game(), cfg);
+    }
 }
